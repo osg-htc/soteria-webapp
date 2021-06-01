b92909a54a8832ad60089820e10bc906174484dc
Brian Aydemir <baydemir@morgridge.org>
2021-06-01 15:10:36 -0500

    Fix routes for debugging

--- registry/debug/blueprints.py
+++ registry/debug/blueprints.py
@@ -5,7 +5,7 @@ Blueprints for debugging the application.
 import json
 import os
 
-from flask import Blueprint, make_response
+from flask import Blueprint, make_response, request
 
 __all__ = ["debugging_bp"]
 
@@ -15,6 +15,11 @@ debugging_bp = Blueprint("debug", __name__)
 @debugging_bp.route("/environ")
 def environ():
     """
-    Returns all available environment variables.
+    Returns environment variables that are available to the current request.
     """
-    return make_response(json.dumps(dict(os.environ), indent=2, sort_keys=True))
+    data = {}
+
+    for k in request.environ:
+        data[k] = str(request.environ[k])
+
+    return make_response(json.dumps(data, indent=2, sort_keys=True))
