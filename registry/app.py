ebedd85b26431667d93c5868a022ccfc1d511c2f
Brian Aydemir <baydemir@morgridge.org>
2021-06-08 10:57:45 -0500

    Load additional config, move logging setup

--- registry/app.py
+++ registry/app.py
@@ -2,6 +2,8 @@
 Web application for managing the users of a Harbor instance.
 """
 
+import logging
+import os
 from pathlib import Path
 
 from flask import Flask
@@ -13,11 +15,66 @@ from .index import index_bp
 
 __all__ = ["create_app"]
 
+THIS_FILE = Path(__file__)
+INSTANCE_DIR = THIS_FILE.parent / "instance"
+LOG_DIR = INSTANCE_DIR / "log"
+
+LOG_FORMAT = "[%(asctime)s] [%(levelname)s] %(module)s:%(lineno)s ~ %(message)s"
+
+
+def setup_logging() -> None:
+    LOG_DIR.mkdir(parents=True, exist_ok=True)
+
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "formatters": {
+                "default": {"format": LOG_FORMAT},
+            },
+            "handlers": {
+                "stream": {
+                    "class": "logging.StreamHandler",
+                    "level": "DEBUG",
+                    "formatter": "default",
+                },
+                "file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "default",
+                    "filename": LOG_DIR / "wsgi.log",
+                    "backupCount": 5,
+                    "maxBytes": 10 * 1024 * 1024,  # 10 MiB
+                },
+            },
+            "root": {"level": "DEBUG", "handlers": ["stream", "file"]},
+        }
+    )
+
+
+def load_config(app: Flask) -> None:
+    pyfiles = Path(app.instance_path).glob("*.py")
+
+    for p in sorted(pyfiles):
+        app.config.from_pyfile(p)
+
+    for key in [
+        "HARBOR_ADMIN_USERNAME",
+        "HARBOR_ADMIN_PASSWORD",
+        "HARBOR_ROBOT_USERNAME",
+        "HARBOR_ROBOT_PASSWORD",
+    ]:
+        val = os.environ.get(key)
+
+        if val:
+            app.config[key] = val
+
 
 def create_app() -> Flask:
+    setup_logging()
+
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config.from_pyfile(Path(app.instance_path) / "config.py", silent=True)
+    load_config(app)
 
     app.register_blueprint(index_bp, url_prefix="/")
     app.register_blueprint(account_bp, url_prefix="/account")
--- wsgi.py
+++ wsgi.py
@@ -1,42 +1,9 @@
 #!/usr/bin/env python3
 
 """
-WSGI application for the `registry` Flask application.
-
-Log files will be placed in the Flask application's instance directory.
+Web application for managing the users of a Harbor instance.
 """
 
-import logging.config
-import pathlib
-
 import registry
-
-THIS_FILE = pathlib.Path(__file__)
-THIS_DIR = THIS_FILE.parent
-LOG_DIR = THIS_DIR / "instance" / "log"
-
-LOG_DIR.mkdir(parents=True, exist_ok=True)
-
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "[%(asctime)s] [%(levelname)s] %(module)s:%(lineno)s ~ %(message)s",
-            },
-        },
-        "handlers": {
-            "file": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "level": "DEBUG",
-                "formatter": "default",
-                "filename": LOG_DIR / "wsgi.log",
-                "backupCount": 5,
-                "maxBytes": 10 * 1024 * 1024,
-            },
-        },
-        "root": {"level": "DEBUG", "handlers": ["file"]},
-    }
-)
 
 application = registry.create_app()
