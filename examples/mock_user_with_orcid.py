e146fa013afc66fe5fda8f2775ce17785dae1e5c
Brian Aydemir <baydemir@morgridge.org>
2021-09-23 12:44:05 -0500

    Mock data needs a good rethink

--- examples/mock_user_with_orcid.py
+++
@@ -1,5 +0,0 @@
-MOCK_USER = {
-    "OIDC_CLAIM_iss": "https://cilogon.org",
-    "OIDC_CLAIM_orcid": "0000-0001-9048-5408",
-    "OIDC_CLAIM_sub": "http://cilogon.org/serverA/users/279521",
-}
--- examples/mock_user_without_orcid.py
+++
@@ -1,4 +0,0 @@
-MOCK_USER = {
-    "OIDC_CLAIM_iss": "https://cilogon.org",
-    "OIDC_CLAIM_sub": "http://cilogon.org/serverB/users/47371797",
-}
